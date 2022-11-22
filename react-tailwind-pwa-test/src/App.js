eb9ed0a983c75bf35ab773bf639050c67f309683
Jimmy Bäckström <jimmy.backstrom@gruppera.se>
2022-11-22 10:05:35 +0100

    get pokemon data and display images and name.

--- react-tailwind-pwa-test/src/App.js
+++ react-tailwind-pwa-test/src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import { getAllPokemonList } from "./api/pokemon";
+import logo from "./logo.svg";
 // import './App.css';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getAllPokemonList();
+      setPokemonData(data?.results);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
-    
+    <div>
+      <div className="mt-50 flex justify-around flex-wrap w-11/12 m-auto">
+        {pokemonData?.map((poke, i) => {
+          return (
+            <div key={i} className="w-[400px] h-[330px] border my-8 mx-2">
+              <div className="p-2">
+                <p className="font-bold capitalize">{poke.name}</p>
+              </div>
+              <img
+                className="h-[300px] w-[300px]"
+                alt="pokemon"
+                src={`https://img.pokemondb.net/artwork/large/${poke.name}.jpg`}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
